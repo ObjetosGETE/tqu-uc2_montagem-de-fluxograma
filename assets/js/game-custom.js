2b05beb7d1b084cb91b1c36e39ed31f9456f6df4
lui <lppbrancher@senacrs.com.br>
2024-05-20 11:04:45 -0300

    feedbacks

--- assets/js/game-custom.js
+++ assets/js/game-custom.js
@@ -7,7 +7,7 @@ var containers = [
 
   // Containers que irão receber os cards
   document.querySelector("#slot-1"),
-  document.querySelector('#slot-2'), 
+  document.querySelector('#slot-2'),
   document.querySelector('#slot-3'),
   document.querySelector('#slot-3'),
   document.querySelector('#slot-4'),
@@ -30,14 +30,14 @@ var erro = 0;
 // Solução ao dragindrop
 var scrollable = true;
 
-var listener = function(e) {
+var listener = function (e) {
   console.log(scrollable)
-    if (! scrollable) {
-        e.preventDefault();
-    }
+  if (!scrollable) {
+    e.preventDefault();
+  }
 }
 
-document.addEventListener('touchmove', listener, { passive:false });
+document.addEventListener('touchmove', listener, { passive: false });
 
 // Solução ao dragindrop
 
@@ -46,42 +46,45 @@ dragula({
   revertOnSpill: true,
   direction: 'vertical',
   accepts: function (el, target, source, sibling) {
-      return el.dataset.target == target.id; 
+    return el.dataset.target == target.id;
   }
-}).on('drag', function(el, source) {
+}).on('drag', function (el, source) {
   // On mobile this prevents the default page scrolling while dragging an item.
   scrollable = false;
-}).on("drop", function(el, target){
+}).on("drop", function (el, target) {
   scrollable = true;
   console.log(el)
   $(el).parents('.elemento').addClass('correto');
   let slots = $(el).parents('.game-container').find('.slot');
   let qtCorretos = $(el).parents('.game-container').find('.correto').length;
-  // mudar if p/ slots.length == qtCorretos
-  if(slots.length == qtCorretos) {
 
-    // implementar feedback positivo
+  if (slots.length == qtCorretos) {
 
+    // feedback positivo
+    const feedback = $(el).parents('.game-container').find('.feedback-positivo');
+    const btnContinuar = $(el).parents('.game-container').find('.btn-continuar')
+    $(feedback).fadeIn();
     let proxTela = $(el).parents('.tela').next();
-    $(el).parents('.tela').fadeOut();
-    proxTela.fadeIn();
-    setTimeout(() => {
-      let msg = proxTela.find('.hidden')[0];
-      $(msg).removeClass('hidden')
-    }, 500);
+    $(btnContinuar).click(function () {
+      $(el).parents('.tela').fadeOut();
+      proxTela.fadeIn();
+      setTimeout(() => {
+        let msg = proxTela.find('.hidden')[0];
+        $(msg).removeClass('hidden')
+      }, 500);
+    })
   }
-  
-  // implementar feedback com audio
-}).on("cancel", function(){
+}).on("cancel", function (el) {
   scrollable = true;
-      // implementar feedback negativo
-
-      // Executa o áudio e a modal necessária
-      // Também é possível fazer algum teste aqui caso necessário.
-  $('#bgmodal-erro').modal('show')
-      audio.setAttribute('src','audios/erro.mp3'); //change the source
-      audio.load(); //load the new source
-      audio.play(); //play
+  // feedback negativo
+  const feedback = $(el).parents('.game-container').find('.feedback-negativo');
+  $(feedback).addClass('active');
+  setTimeout(() => {
+    $(feedback).removeClass('active');
+  }, 3000);
+  audio.setAttribute('src', 'audios/erro.mp3'); //change the source
+  audio.load(); //load the new source
+  audio.play(); //play
 });
 
 // document.addEventListener('touchmove', function(e) { e.preventDefault(); }, { passive:false });
