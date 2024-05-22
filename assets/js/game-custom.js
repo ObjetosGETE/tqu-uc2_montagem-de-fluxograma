0883e2046028bab5bd9643199118799344cdfb67
lui <lppbrancher@senacrs.com.br>
2024-05-22 11:41:13 -0300

    funcao reiniciar atividade

--- assets/js/game-custom.js
+++ assets/js/game-custom.js
@@ -38,53 +38,90 @@ var listener = function (e) {
 }
 
 document.addEventListener('touchmove', listener, { passive: false });
-
+let drake;
 // Solução ao dragindrop
+function setupDragula() {
+  console.log('setting dragula up')
+  drake = dragula({
+    containers: containers,
+    revertOnSpill: true,
+    direction: 'vertical',
+    accepts: function (el, target, source, sibling) {
+      return el.dataset.target == target.id;
+    }
+  }).on('drag', function (el, source) {
+    // On mobile this prevents the default page scrolling while dragging an item.
+    scrollable = false;
+  }).on("drop", function (el, target) {
+    scrollable = true;
+    $(el).parents('.elemento').addClass('correto');
+    let slots = $(el).parents('.game-container').find('.slot');
+    let qtCorretos = $(el).parents('.game-container').find('.correto').length;
 
-dragula({
-  containers: containers,
-  revertOnSpill: true,
-  direction: 'vertical',
-  accepts: function (el, target, source, sibling) {
-    return el.dataset.target == target.id;
-  }
-}).on('drag', function (el, source) {
-  // On mobile this prevents the default page scrolling while dragging an item.
-  scrollable = false;
-}).on("drop", function (el, target) {
-  scrollable = true;
-  $(el).parents('.elemento').addClass('correto');
-  let slots = $(el).parents('.game-container').find('.slot');
-  let qtCorretos = $(el).parents('.game-container').find('.correto').length;
+    if (slots.length > 1) {
 
-  if (slots.length == qtCorretos) {
-
-    // feedback positivo
-    const feedback = $(el).parents('.game-container').find('.feedback-positivo');
-    const btnContinuar = $(el).parents('.game-container').find('.btn-continuar')
-    $(feedback).fadeIn();
-    let proxTela = $(el).parents('.tela').next();
-    console.log(proxTela)
-    $(btnContinuar).click(function () {
-      $(el).parents('.tela').fadeOut();
-      $(proxTela).fadeIn();
-      setTimeout(() => {
-        let msg = proxTela.find('.hidden')[0];
-        $(msg).removeClass('hidden')
-      }, 500);
-    })
-  }
-}).on("cancel", function (el) {
-  scrollable = true;
-  // feedback negativo
-  const feedback = $(el).parents('.game-container').find('.feedback-negativo');
-  $(feedback).addClass('active');
-  setTimeout(() => {
-    $(feedback).removeClass('active');
-  }, 3000);
-  audio.setAttribute('src', 'audios/erro.mp3'); //change the source
-  audio.load(); //load the new source
-  audio.play(); //play
-});
+      // feedback positivo
+      const feedback = $(el).parents('.game-container').find('.feedback-positivo');
+      const btnContinuar = $(el).parents('.game-container').find('.btn-continuar')
+      $(feedback).fadeIn();
+      let proxTela = $(el).parents('.tela').next();
+      console.log(proxTela)
+      $(btnContinuar).click(function () {
+        $(el).parents('.tela').fadeOut();
+        $(proxTela).fadeIn();
+        setTimeout(() => {
+          let msg = proxTela.find('.hidden')[0];
+          $(msg).removeClass('hidden')
+        }, 500);
+      })
+    }
+  }).on("cancel", function (el) {
+    scrollable = true;
+    // feedback negativo
+    const feedback = $(el).parents('.game-container').find('.feedback-negativo');
+    $(feedback).addClass('active');
+    setTimeout(() => {
+      $(feedback).removeClass('active');
+    }, 3000);
+    audio.setAttribute('src', 'audios/erro.mp3'); //change the source
+    audio.load(); //load the new source
+    audio.play(); //play
+  });
+}
+setupDragula();
 
 // document.addEventListener('touchmove', function(e) { e.preventDefault(); }, { passive:false });
+function resetCards() {
+  $('.correto').removeClass('correto');
+  const arrayFluxograma1 = $('.fluxograma-1.game-container .slot .card');
+  const arrayFluxograma2 = $('.fluxograma-1.game-container .slot .card');
+  for (let index = 0; index < arrayFluxograma1.length; index++) {
+    console.log($('.fluxograma-1.game-container .slot .card')[index])
+    $('.fluxograma-1 .card-container')[0].append($('.fluxograma-1.game-container .slot .card')[0])
+  };
+  for (let index = 0; index < arrayFluxograma1.length; index++) {
+    console.log($('.fluxograma-2.game-container .slot .card')[index])
+    $('.fluxograma-2 .card-container')[0].append($('.fluxograma-2.game-container .slot .card')[0])
+  };
+}
+
+$('.btn-restart').click(function () {
+  resetCards();
+  $('.feedback-positivo').fadeOut();
+  $('.fluxograma-1.game-container').fadeOut();
+  $('.fluxograma-1.modelo').fadeIn();
+  $('.fluxograma-2.game-container').fadeOut();
+  $('.fluxograma-2.modelo').fadeIn();
+  $('.tela-final').fadeOut();
+  $('.tela-1 .msg').addClass('hidden');
+  $('.tela-2 .msg').addClass('hidden');
+  $('.loading').removeClass('loading');
+
+  setTimeout(() => {
+    $('.tela-inicial').fadeIn();
+    $('.tela-1 .backdrop').fadeIn();   
+    $('.tela-2 .backdrop').fadeIn();
+    $('.tela-inicial .backdrop').fadeIn();
+    $('.tela-1').fadeIn();
+  }, 500);
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,14 @@ $('.btn-start').click(function () {
     $('.tela-inicial').fadeOut();
     $('.tela-1 .msg').removeClass('hidden');
 })
-$('.btn-restart').click(function () { location.reload() })
+
 $('.start-fluxograma').click(function () {
     const tela = $(this).parents('.tela');
-    const contador = $(tela).find('.load')
-    const fluxogramaModelo = $(tela).find('.modelo')
-    const gameContainer = $(tela).find('.game-container')
+    const contador = $(tela).find('.load');
+    const fluxogramaModelo = $(tela).find('.modelo');
+    const gameContainer = $(tela).find('.game-container');
     $(tela).children('.backdrop').fadeOut();
-    $(contador).addClass('loading')
+    $(contador).addClass('loading');
     setTimeout(() => {
         $(fluxogramaModelo).fadeOut();
         setTimeout(() => {
